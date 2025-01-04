d9941f023ceacca9d430b9fab3ad5bc5da22a44e
zeronosu77108 <taiga77108@gmail.com>
2025-01-04 09:10:31 +0900

    Add RSpec support configs for testing tools integration
    Introduce configurations for RSpec to integrate with FactoryBot, Shoulda Matchers, Rails Controller Testing, and SimpleCov. These additions enhance the testing setup by simplifying syntax, enabling feature-rich matchers, and ensuring test coverage monitoring.

---
+++ spec/support/controller_testing.rb
@@ -0,0 +1,5 @@
+RSpec.configure do |config|
+  config.include Rails::Controller::Testing::TestProcess
+  config.include Rails::Controller::Testing::TemplateAssertions
+  config.include Rails::Controller::Testing::Integration
+end
